b35a232717d2d73037984522de4178efc6749af8
Filipe Teixeira Terto <filipe.terto@fatec.sp.gov.br>
2026-02-08 18:42:56 -0300

    Ajustes de erros no projeto

--- Front-End/api/api.js
+++ Front-End/api/api.js
@@ -1,8 +1,6 @@
-import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "http://localhost:3001/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
